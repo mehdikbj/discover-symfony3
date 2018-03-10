0d4c686a5983a3f842fb73435527a7f291071f05
mehdi kabbaj <mka@octo.com>
2018-03-10 21:54:18 +0000

    config route

--- src/AppBundle/Controller/MyController.php
+++ src/AppBundle/Controller/MyController.php
@@ -4,8 +4,21 @@ namespace AppBundle\Controller;
 
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Symfony\Component\HttpFoundation\Request;
+
 
 class MyController extends Controller
 {
+
+    /**
+     * @Route("/my/{params}", name="my", defaults={"params"=null})
+     * @Method("GET")
+     */
+    public function myAction(Request $request) {
+
+        return $this->render('myTemplate/my.html.twig', ['name' => $request->get('params')]);
+    }
 
 }
